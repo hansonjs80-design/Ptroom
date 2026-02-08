6854f70bd6dae41633179dd86619010f7466f70d
JOO HANSOL <joohansol@JOOui-MacBookPro.local>
2026-02-08 13:37:04 +0900

    style: further adjust status icon area position on mobile portrait

--- components/bed-card/BedNumberAndStatus.tsx
+++ components/bed-card/BedNumberAndStatus.tsx
@@ -33,7 +33,7 @@ export const BedNumberAndStatus: React.FC<BedNumberAndStatusProps> = memo(({ bed
 
       {/* Status Icons Area */}
       <div
-        className={`flex items-center cursor-pointer p-2 lg:p-3 w-[12px] lg:w-auto h-10 lg:h-auto ${!isDesktop ? (hasStatus ? '-translate-x-[8px]' : '-translate-x-[4px]') : 'lg:translate-x-0'} lg:pr-5 rounded-lg hover:bg-black/5 dark:hover:bg-white/5 transition-all overflow-visible`}
+        className={`flex items-center cursor-pointer p-2 lg:p-3 w-[12px] lg:w-auto h-10 lg:h-auto ${!isDesktop ? (hasStatus ? '-translate-x-[11px]' : 'translate-x-0') : 'lg:translate-x-0'} lg:pr-5 rounded-lg hover:bg-black/5 dark:hover:bg-white/5 transition-all overflow-visible`}
         onClick={isDesktop ? onEditStatus : undefined}
         onDoubleClick={!isDesktop ? onEditStatus : undefined}
         title={isDesktop ? "클릭하여 상태 아이콘 설정" : "더블클릭하여 상태 아이콘 설정"}
